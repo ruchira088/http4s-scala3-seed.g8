a51a9cf38242a83829498c024b0cad622c73fb82
Ruchira (Richie) Jayasekara <ruchira088@gmail.com>
2018-04-17 21:34:12 +1000

    Upgrading dependency package versions

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies
 {
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.4"
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.5"
 
   lazy val pegdown = "org.pegdown" % "pegdown" % "1.6.0"
 }

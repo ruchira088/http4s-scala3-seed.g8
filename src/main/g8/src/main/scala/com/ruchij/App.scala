c17977f0263214c31b02c0f974e761da1a11c64b
Ruchira (Richie) Jayasekara <ruchira088@gmail.com>
2019-03-29 23:57:03 +1100

    Removing unused function

--- src/main/g8/src/main/scala/com/ruchij/App.scala
+++ src/main/g8/src/main/scala/com/ruchij/App.scala
@@ -2,10 +2,7 @@ package com.ruchij
 
 object App
 {
-  def main(args: Array[String]): Unit =
-  {
+  def main(args: Array[String]): Unit = {
     println("Hello World")
   }
-
-  def add(x: Int, y: Int): Int = x + y
 }
--- src/main/g8/src/test/scala/com/ruchij/ExampleTest.scala
+++ src/main/g8/src/test/scala/com/ruchij/ExampleTest.scala
@@ -2,8 +2,7 @@ package com.ruchij
 
 import org.scalatest.{FlatSpec, MustMatchers}
 
-class ExampleTest extends FlatSpec with MustMatchers
-{
+class ExampleTest extends FlatSpec with MustMatchers {
   "Sample test" should "pass nicely" in {
     8 mustBe 8
   }

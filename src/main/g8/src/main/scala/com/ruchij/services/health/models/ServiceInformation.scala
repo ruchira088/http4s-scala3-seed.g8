d784ca3128be886c850d2fea2979539bf6b31816
Ruchira (Richie) Jayasekara <me@ruchij.com>
2020-04-18 15:40:16 +1000

    Refactoring

--- src/main/g8/src/main/scala/com/ruchij/services/health/models/ServiceInformation.scala
+++ src/main/g8/src/main/scala/com/ruchij/services/health/models/ServiceInformation.scala
@@ -1,6 +1,8 @@
 package com.ruchij.services.health.models
 
 import cats.Applicative
+import cats.effect.Sync
+import cats.implicits._
 import com.eed3si9n.ruchij.BuildInfo
 import com.ruchij.circe.Encoders.dateTimeEncoder
 import io.circe.generic.auto._
@@ -11,27 +13,30 @@ import org.joda.time.DateTime
 import scala.util.Properties
 
 case class ServiceInformation(
-  serviceName: String,
-  serviceVersion: String,
-  organization: String,
-  scalaVersion: String,
-  sbtVersion: String,
-  javaVersion: String,
-  timestamp: DateTime
-)
+                               serviceName: String,
+                               serviceVersion: String,
+                               organization: String,
+                               scalaVersion: String,
+                               sbtVersion: String,
+                               javaVersion: String,
+                               timestamp: DateTime
+                             )
 
 object ServiceInformation {
   implicit def serviceInformationEncoder[F[_]: Applicative]: EntityEncoder[F, ServiceInformation] =
     jsonEncoderOf[F, ServiceInformation]
 
-  def apply(timestamp: DateTime): ServiceInformation =
-    ServiceInformation(
-      BuildInfo.name,
-      BuildInfo.version,
-      BuildInfo.organization,
-      BuildInfo.scalaVersion,
-      BuildInfo.sbtVersion,
-      Properties.javaVersion,
-      timestamp
-    )
+  def create[F[_]: Sync](timestamp: DateTime): F[ServiceInformation] =
+    Sync[F].delay(Properties.javaVersion)
+      .map { javaVersion =>
+        ServiceInformation(
+          BuildInfo.name,
+          BuildInfo.version,
+          BuildInfo.organization,
+          BuildInfo.scalaVersion,
+          BuildInfo.sbtVersion,
+          javaVersion,
+          timestamp
+        )
+      }
 }

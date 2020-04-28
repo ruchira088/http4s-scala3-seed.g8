7a752d63b93067f34e4b7028b022b881473006c8
Ruchira (Richie) Jayasekara <me@ruchij.com>
2020-04-29 02:43:39 +1000

    Refactoring

--- src/main/g8/src/main/scala/com/ruchij/types/FunctionKTypes.scala
+++ src/main/g8/src/main/scala/com/ruchij/types/FunctionKTypes.scala
@@ -1,13 +1,13 @@
 package com.ruchij.types
 
-import cats.{Applicative, MonadError, ~>}
+import cats.{Applicative, ApplicativeError, ~>}
 
 object FunctionKTypes {
 
-  implicit def eitherToF[L, F[_]: MonadError[*[_], L]]: Either[L, *] ~> F =
+  implicit def eitherToF[L, F[_]: ApplicativeError[*[_], L]]: Either[L, *] ~> F =
     new ~>[Either[L, *], F] {
       override def apply[A](either: Either[L, A]): F[A] =
-        either.fold(MonadError[F, L].raiseError, Applicative[F].pure)
+        either.fold(ApplicativeError[F, L].raiseError, Applicative[F].pure)
     }
 
   def identityFunctionK[F[_]]: F ~> F = new ~>[F, F] {

0e563dbed42006a85de2536c97e1e83688d5a498
Ruchira (Richie) Jayasekara <me@ruchij.com>
2020-06-03 18:23:22 +1000

    Updating dependencies

--- src/main/g8/src/main/scala/com/ruchij/services/health/models/ServiceInformation.scala
+++ src/main/g8/src/main/scala/com/ruchij/services/health/models/ServiceInformation.scala
@@ -13,14 +13,14 @@ import org.joda.time.DateTime
 import scala.util.Properties
 
 case class ServiceInformation(
-                               serviceName: String,
-                               serviceVersion: String,
-                               organization: String,
-                               scalaVersion: String,
-                               sbtVersion: String,
-                               javaVersion: String,
-                               timestamp: DateTime
-                             )
+  serviceName: String,
+  serviceVersion: String,
+  organization: String,
+  scalaVersion: String,
+  sbtVersion: String,
+  javaVersion: String,
+  timestamp: DateTime
+)
 
 object ServiceInformation {
   implicit def serviceInformationEncoder[F[_]: Applicative]: EntityEncoder[F, ServiceInformation] =

e596432e094e0a610b03034ea6fb267e0333a734
Ruchira (Richie) Jayasekara <me@ruchij.com>
2020-09-22 18:26:14 +1000

    Upgrading pureconfig to 0.14.0

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -20,7 +20,7 @@ object Dependencies
 
   lazy val jodaTime = "joda-time" % "joda-time" % "2.10.6"
 
-  lazy val pureconfig = "com.github.pureconfig" %% "pureconfig" % "0.13.0"
+  lazy val pureconfig = "com.github.pureconfig" %% "pureconfig" % "0.14.0"
 
   lazy val logbackClassic = "ch.qos.logback" % "logback-classic" % "1.2.3"
 

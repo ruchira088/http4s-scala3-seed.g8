e39860f6725b5c259350132844772b36ddc7f15d
Ruchira (Richie) Jayasekara <me@ruchij.com>
2020-12-17 18:40:35 +1100

    Upgrading unit test

--- src/main/g8/src/test/scala/com/ruchij/web/routes/HealthRoutesSpec.scala
+++ src/main/g8/src/test/scala/com/ruchij/web/routes/HealthRoutesSpec.scala
@@ -31,7 +31,7 @@ class HealthRoutesSpec extends AnyFlatSpec with Matchers {
         "serviceVersion": \${BuildInfo.version},
         "organization": "com.ruchij",
         "scalaVersion": "2.13.4",
-        "sbtVersion": "1.4.4",
+        "sbtVersion": "1.4.5",
         "gitBranch" : "test-branch",
         "gitCommit" : "my-commit",
         "javaVersion": \${Properties.javaVersion},

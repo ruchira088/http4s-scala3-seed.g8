67f2f95df661705b95a9ed01af163ff25956e778
Ruchira (Richie) Jayasekara <me@ruchij.com>
2021-02-06 15:11:23 +1100

    Upgrading joda-time to 2.10.10

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -18,7 +18,7 @@ object Dependencies
 
   lazy val circeLiteral = "io.circe" %% "circe-literal" % CirceVersion
 
-  lazy val jodaTime = "joda-time" % "joda-time" % "2.10.9"
+  lazy val jodaTime = "joda-time" % "joda-time" % "2.10.10"
 
   lazy val pureconfig = "com.github.pureconfig" %% "pureconfig" % "0.14.0"
 

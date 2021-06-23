3a515897338682808919d92efa5b8eebc672197c
Ruchira (Richie) Jayasekara <me@ruchij.com>
2021-06-23 16:05:39 +1000

    Upgrading to sbt 1.5.4

--- src/main/g8/src/test/scala/com/ruchij/web/routes/HealthRoutesSpec.scala
+++ src/main/g8/src/test/scala/com/ruchij/web/routes/HealthRoutesSpec.scala
@@ -28,7 +28,7 @@ class HealthRoutesSpec extends AnyFlatSpec with Matchers {
         "serviceVersion": \${BuildInfo.version},
         "organization": "com.ruchij",
         "scalaVersion": "2.13.6",
-        "sbtVersion": "1.5.3",
+        "sbtVersion": "1.5.4",
         "javaVersion": \${Properties.javaVersion},
         "gitBranch" : "test-branch",
         "gitCommit" : "my-commit",

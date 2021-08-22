c93821669db143f2795b760942951473423a43d0
Ruchira (Richie) Jayasekara <me@ruchij.com>
2021-08-22 20:35:24 +1000

    Refactoring

--- src/main/g8/src/test/scala/com/ruchij/test/utils/JsonUtils.scala
+++ src/main/g8/src/test/scala/com/ruchij/test/utils/JsonUtils.scala
@@ -4,13 +4,13 @@ import cats.effect.Sync
 import cats.implicits._
 import com.ruchij.types.FunctionKTypes._
 import io.circe.Json
-import io.circe.parser.{parse => parseJson}
+import io.circe.{parser => JsonParser}
 import org.http4s.Response
 
 object JsonUtils {
   def fromResponse[F[_]: Sync](response: Response[F]): F[Json] =
     response.bodyText.compile[F, F, String].string
       .flatMap {
-        text => parseJson(text).toType[F, Throwable]
+        text => JsonParser.parse(text).toType[F, Throwable]
       }
 }

eae65c80388277b5f0d45b2e198b567cd49fc244
Ruchira (Richie) Jayasekara <me@ruchij.com>
2021-10-05 20:26:03 +1100

    Upgrading dependencies

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -18,9 +18,9 @@ object Dependencies
 
   lazy val circeLiteral = "io.circe" %% "circe-literal" % CirceVersion
 
-  lazy val jodaTime = "joda-time" % "joda-time" % "2.10.11"
+  lazy val jodaTime = "joda-time" % "joda-time" % "2.10.12"
 
-  lazy val pureconfig = "com.github.pureconfig" %% "pureconfig" % "0.16.0"
+  lazy val pureconfig = "com.github.pureconfig" %% "pureconfig" % "0.17.0"
 
   lazy val logbackClassic = "ch.qos.logback" % "logback-classic" % "1.2.6"
 

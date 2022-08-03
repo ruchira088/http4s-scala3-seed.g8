497c99db9e77930a21331ccad20827d538a7764c
Ruchira (Richie) Jayasekara <me@ruchij.com>
2022-08-04 00:26:58 +1000

    Upgrading ScalaTest to 3.2.13

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -30,7 +30,7 @@ object Dependencies
 
   lazy val betterMonadicFor = "com.olegpy" %% "better-monadic-for" % "0.3.1"
 
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.12"
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.13"
 
   lazy val scalaMock = "org.scalamock" %% "scalamock" % "5.2.0"
 

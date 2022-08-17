cbc25cb032f3310dc4f5a12e3f112166ae28b0b2
Ruchira (Richie) Jayasekara <me@ruchij.com>
2022-08-17 15:25:27 +1000

    Updating to Scala 3 syntax

--- src/main/g8/src/main/scala/com/ruchij/types/FunctionKTypes.scala
+++ src/main/g8/src/main/scala/com/ruchij/types/FunctionKTypes.scala
@@ -4,14 +4,14 @@ import cats.{Applicative, ApplicativeError, ~>}
 
 object FunctionKTypes {
 
-  implicit class FunctionK2TypeOps[F[+_, +_], A, B](value: F[B, A]) {
+  extension [F[+_, +_], A, B] (value: F[B, A]) {
     def toType[G[_], C >: B](using functionK: ~>[[X] =>> F[C, X], G]): G[A] = functionK(value)
   }
 
-  implicit def eitherToF[L, F[_]: [G[_]] =>> ApplicativeError[G, L]]: ~>[[A] =>> Either[L, A], F] =
-    new ~>[[A] =>> Either[L, A], F] {
-      override def apply[A](either: Either[L, A]): F[A] =
-        either.fold(ApplicativeError[F, L].raiseError, Applicative[F].pure)
-    }
+  given [L, F[_]: [G[_]] =>> ApplicativeError[G, L]]: ~>[[A] =>> Either[L, A], F] =
+  new ~>[[A] =>> Either[L, A], F] {
+    override def apply[A](either: Either[L, A]): F[A] =
+      either.fold(ApplicativeError[F, L].raiseError, Applicative[F].pure)
+  }
 
 }
--- src/main/g8/src/test/scala/com/ruchij/test/utils/JsonUtils.scala
+++ src/main/g8/src/test/scala/com/ruchij/test/utils/JsonUtils.scala
@@ -1,8 +1,8 @@
 package com.ruchij.test.utils
 
 import cats.effect.Sync
-import cats.implicits._
-import com.ruchij.types.FunctionKTypes._
+import cats.implicits.*
+import com.ruchij.types.FunctionKTypes.{given, *}
 import io.circe.Json
 import io.circe.{parser => JsonParser}
 import org.http4s.Response

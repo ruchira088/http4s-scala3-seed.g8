436a5e87843c362a560b8cd256f9c0b531b98db4
Ruchira (Richie) Jayasekara <me@ruchij.com>
2022-09-15 14:26:06 +1000

    Making the extension HKT invariant

--- src/main/g8/src/main/scala/com/ruchij/types/FunctionKTypes.scala
+++ src/main/g8/src/main/scala/com/ruchij/types/FunctionKTypes.scala
@@ -1,17 +1,23 @@
 package com.ruchij.types
 
-import cats.{Applicative, ApplicativeError, ~>}
+import cats.{Applicative, ApplicativeError, Functor, ~>}
 
 object FunctionKTypes {
 
-  extension [F[+_, +_], A, B] (value: F[B, A]) {
-    def toType[G[_], C >: B](using functionK: ~>[[X] =>> F[C, X], G]): G[A] = functionK(value)
+  extension [F[_, _], A, B] (value: F[B, A]) {
+    def toType[G[_], C >: B](using functionK: ~>[[X] =>> F[C, X], G], functor: Functor[[X] =>> F[X, A]]): G[A] =
+      functionK(functor.map(value)(identity[C]))
   }
 
+  given [R]: Functor[[X] =>> Either[X, R]] =
+    new Functor[[X] =>> Either[X, R]] {
+      override def map[A, B](fa: Either[A, R])(f: A => B): Either[B, R] = fa.left.map(f)
+    }
+
   given [L, F[_]: [G[_]] =>> ApplicativeError[G, L]]: ~>[[A] =>> Either[L, A], F] =
-  new ~>[[A] =>> Either[L, A], F] {
-    override def apply[A](either: Either[L, A]): F[A] =
-      either.fold(ApplicativeError[F, L].raiseError, Applicative[F].pure)
-  }
+    new ~>[[A] =>> Either[L, A], F] {
+      override def apply[A](either: Either[L, A]): F[A] =
+        either.fold(ApplicativeError[F, L].raiseError, Applicative[F].pure)
+    }
 
 }

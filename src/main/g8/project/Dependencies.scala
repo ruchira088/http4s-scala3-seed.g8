5e5547e555b84b158207418027a3bbca7042c5c8
Ruchira (Richie) Jayasekara <me@ruchij.com>
2023-05-14 01:49:27 +1000

    Upgrading dependencies

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies
 {
   val ScalaVersion = "3.2.2"
-  val Http4sVersion = "0.23.18"
+  val Http4sVersion = "0.23.19"
   val CirceVersion = "0.14.5"
 
   lazy val http4sDsl = "org.http4s" %% "http4s-dsl" % Http4sVersion
@@ -20,7 +20,7 @@ object Dependencies
 
   lazy val jodaTime = "joda-time" % "joda-time" % "2.12.5"
 
-  lazy val pureconfig = "com.github.pureconfig" %% "pureconfig-core" % "0.17.3"
+  lazy val pureconfig = "com.github.pureconfig" %% "pureconfig-core" % "0.17.4"
 
   lazy val logbackClassic = "ch.qos.logback" % "logback-classic" % "1.4.7"
 

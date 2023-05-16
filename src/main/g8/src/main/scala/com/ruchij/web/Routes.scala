8155706597666f96cb584bff29a6aa055546c653
Ruchira (Richie) Jayasekara <me@ruchij.com>
2023-05-17 00:10:15 +1000

    Updating deprecated code

--- src/main/g8/src/main/scala/com/ruchij/web/Routes.scala
+++ src/main/g8/src/main/scala/com/ruchij/web/Routes.scala
@@ -4,13 +4,14 @@ import cats.effect.Sync
 import com.ruchij.services.health.HealthService
 import com.ruchij.web.middleware.{ExceptionHandler, NotFoundHandler}
 import com.ruchij.web.routes.ServiceRoutes
+import fs2.compression.Compression
 import org.http4s.dsl.Http4sDsl
 import org.http4s.server.Router
 import org.http4s.server.middleware.GZip
 import org.http4s.{HttpApp, HttpRoutes}
 
 object Routes {
-  def apply[F[_]: Sync](healthService: HealthService[F]): HttpApp[F] = {
+  def apply[F[_]: Sync: Compression](healthService: HealthService[F]): HttpApp[F] = {
     given Http4sDsl[F] = new Http4sDsl[F] {}
 
     val routes: HttpRoutes[F] =
--- src/main/g8/src/test/scala/com/ruchij/test/mixins/MockedRoutes.scala
+++ src/main/g8/src/test/scala/com/ruchij/test/mixins/MockedRoutes.scala
@@ -3,6 +3,7 @@ package com.ruchij.test.mixins
 import cats.effect.kernel.Sync
 import com.ruchij.services.health.HealthService
 import com.ruchij.web.Routes
+import fs2.compression.Compression
 import org.http4s.HttpApp
 import org.scalatest.{OneInstancePerTest, Suite}
 import org.scalatestplus.mockito.MockitoSugar
@@ -12,8 +13,9 @@ trait MockedRoutes[F[_]] extends MockitoSugar with OneInstancePerTest {  self: S
   val healthService: HealthService[F] = mock[HealthService[F]]
 
   val sync: Sync[F]
+  val compression: Compression[F]
 
   def createRoutes: HttpApp[F] =
-    Routes[F](healthService)(sync)
+    Routes[F](healthService)(sync, compression)
 
 }

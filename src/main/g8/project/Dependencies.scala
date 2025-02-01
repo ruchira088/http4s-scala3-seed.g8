da5a40132bb46bc32f8e7e41092b2a3c0af69c1a
Ruchira (Richie) Jayasekara <me@ruchij.com>
2025-02-01 11:25:11 +1100

    Upgrading dependencies and GitHub Actions

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -22,7 +22,7 @@ object Dependencies
 
   lazy val pureconfig = "com.github.pureconfig" %% "pureconfig-core" % "0.17.8"
 
-  lazy val logbackClassic = "ch.qos.logback" % "logback-classic" % "1.5.15"
+  lazy val logbackClassic = "ch.qos.logback" % "logback-classic" % "1.5.16"
 
   lazy val scalaLogging = "com.typesafe.scala-logging" %% "scala-logging" % "3.9.5"
 
